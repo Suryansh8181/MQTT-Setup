4ebd6351281d3e4c847817b453d3f9d3f869c0f6
Suryansh Baranwal <suryanshb123@gmail.com>
2023-11-26 13:42:29 +0530

    Update Authentication.java

--- Authentication.java
+++ Authentication.java
@@ -1,21 +1,44 @@
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Map;
+
 @RestController
 public class AuthenticationController {
 
-    @Autowired
-    private TokenService tokenService;
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
-    @PostMapping("/")
+    @PostMapping("/generate-token")
     public ResponseEntity<String> generateToken(@RequestBody Map<String, String> requestBody) {
         String email = requestBody.get("email");
 
-        // Validate email
-        if (email == null || !email.matches("@gmail.com")) {
-            return ResponseEntity.badRequest().body("Invalid email format");
+        if (email == null || email.isEmpty()) {
+            return ResponseEntity.badRequest().body("Email is required");
         }
 
-        // Generate token and set expiration to 5 minutes
-        String token = tokenService.generateToken(email, 5 * 60 * 1000);
+        String token = generateJWTToken(email);
 
         return ResponseEntity.ok(token);
+    }
+
+    
+    private String generateJWTToken(String email) {
+        long currentTimeMillis = System.currentTimeMillis();
+        long expirationTime = currentTimeMillis + 5 * 60 * 1000; 
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(currentTimeMillis))
+                .setExpiration(new Date(expirationTime))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
     }
 }
